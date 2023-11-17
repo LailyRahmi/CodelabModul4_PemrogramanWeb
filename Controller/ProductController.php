fe2dd03c9b8a97912938a2aa765326f345a7d769
LailyAuliaRahmi <lailyauliarahmi@webmail.umm.ac.id>
2023-11-18 01:16:37 +0700

    codelab4

---
+++ Controller/ProductController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Controller;
+
+use Traits\ResponseFormatter;
+
+include "Traits/ResponseFormatter.php";
+include "Controller/Controller.php";
+
+class ProductController extends Controller{
+    use ResponseFormatter;
+
+    public function __construct()
+    {
+        $this -> controllerName = "Get All Product";
+        $this -> controllerMethod = "GET";
+    }
+
+    public function getAllProduct(){
+        $dummyData = [
+            "Air Mineral",
+            "Kebab",
+            "Spaghetti",
+            "Jus Jambu"
+        ];
+
+        $response = [
+            "controller_attribute" => $this -> getControllerAttribute(),
+            "product" => $dummyData
+        ];
+
+        return $this -> responseFormatter(200, "Success", $response);
+    }
+}
